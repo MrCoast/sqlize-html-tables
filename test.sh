fc6cbcaa99731240cb000ac311e40a0b4a6261ba
Victor Karalkin <mr.coast72@gmail.com>
2018-12-06 00:10:00 +0300

    Add lint and test scripts

---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if ! [[ $(docker ps -q --filter="name=sqlizehtmltables_webpack_1") ]]; then
+    echo "docker isn't running, run docker-compose up"
+    exit 1
+fi
+
+docker exec -it $(docker ps -q --filter="name=sqlizehtmltables_webpack_1") node_modules/grunt/bin/grunt karma:all
